bec4c22aef23de8facb54839e281f731089cbd94
jfri2 <jfritz022@gmail.com>
2016-03-06 18:09:18 -0500

    Fixed comments in can.c and can.h

--- can.c
+++ can.c
@@ -1,67 +1,14 @@
-/** @file console.c
- *  @brief A console driver.
+/** @file can.c
+ *  @brief A CAN driver.
  *
- *  These empty function definitions are provided
- *  so that stdio will build without complaining.
- *  You will need to fill these functions in. This
- *  is the implementation of the console driver.
- *  Important details about its implementation
- *  should go in these comments.
+ *  comments go here
  *
- *  @author Harry Q. Bovik (hqbovik)
- *  @author Fred Hacker (fhacker)
- *  @bug No know bugs.
+ *  @author John Fritz
+ *  @bug Not Finished
  */
 
-#include <console.h>
+//#include <console.h>
 
-int
-putbyte( char ch )
-{
+int putbyte( char ch ) {
   return ch;
-}
-
-void 
-putbytes( const char *s, int len )
-{
-}
-
-void
-set_term_color( int color )
-{
-}
-
-void
-get_term_color( int *color )
-{
-}
-
-void
-set_cursor( int row, int col )
-{
-}
-
-void
-get_cursor( int *row, int *col )
-{
-}
-
-void
-hide_cursor()
-{
-}
-
-void
-show_cursor()
-{
-}
-
-void 
-clear_console()
-{
-}
-
-void
-draw_char( int row, int col, int ch, int color )
-{
 }
